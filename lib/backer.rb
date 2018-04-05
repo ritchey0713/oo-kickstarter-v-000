fa8019ecedfa4f7db0808b13308199dfe4759a8e
David Ritchey <ritchey0713@gmail.com>
2018-04-05 21:50:28 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -14,8 +14,8 @@ class Backer
     @@backed_projects 
   end
   
-  def back_project(Project)
-     new_project = Project.new(name)
+  def back_project(project)
+     new_project = Project.new(project)
     backed_projects << new_project 
    # binding.pry
     end
